0816bb1834fe9af1386e980b265566412dbd1ee0
Brenton Lin <notnerb.94@ufl.edu>
2017-10-25 01:34:11 -0400

    minor things

---
+++ src/PeerManager.java
@@ -0,0 +1,7 @@
+//Sorry for not showing up last evening Chase, CEN took a bit longer than I really wanted, but it's done for now.
+
+//Concept: Class should provide peers with a manager that chokes and unchokes peers based on project specification
+
+//Possibly pass in the already constructed PeerInfoList
+
+//will need to discuss what we'd like this to do.
