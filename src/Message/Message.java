a5224923dc36252acd432b986f31d78d2a37428f
timami <timami@ufl.edu>
2017-10-25 21:56:56 -0400

    Create message folder and class

---
+++ src/Message/Message.java
@@ -0,0 +1,17 @@
+package Message;
+
+public class Message {
+    private int length;
+    private int type; //0-7
+    private byte[] payload;
+
+    public Message(int length, int type, byte[] payload) { //4-7
+        this.length = length;
+        this.type = type;
+        this.payload = payload;
+    }
+    public Message(int length, int type) { //0-3
+        this.length = length;
+        this.type = type;
+    }
+}
