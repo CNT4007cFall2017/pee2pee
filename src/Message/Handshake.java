4f1339b9c6d68d90f0707e262994163851305dc7
timami <timami@ufl.edu>
2017-10-26 20:41:18 -0400

    Fill handshake class

--- src/Message/Handshake.java
+++ src/Message/Handshake.java
@@ -1,4 +1,13 @@
 package Message;
 
 public class Handshake {
+    private String header;
+    private byte[] zero;
+    private byte[] peerID;
+
+    public Handshake(byte[] peerID) {
+        this.header = "P2PFILESHARINGPROJ";
+        this.zero = new byte[8];
+        this.peerID = peerID;
+    }
 }
