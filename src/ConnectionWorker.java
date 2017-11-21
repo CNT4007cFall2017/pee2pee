8d722ed99b63b6b4de8e9be0303df197b3d0c17d
Brenton Lin <notnerb.94@ufl.edu>
2017-11-20 22:37:55 -0500

    Started the refactor train rolling with new Connectionworker and handling of incoming connections

--- src/ConnectionWorker.java
+++ src/ConnectionWorker.java
@@ -1,19 +1,55 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import Message.*;
 import java.util.Set;
 
 public class ConnectionWorker implements Runnable {
 
     private Socket socket;
     private boolean receivedHandshake;
+    private ObjectOutputStream output;
+    private ObjectInputStream input;
+    private int remoteId;
+    private PeerInfo peerInfo;
 
 
-    public ConnectionWorker(Socket socket) {
+    public ConnectionWorker(Socket socket, PeerInfo peerInfo) {
         receivedHandshake = false;
         this.socket = socket;
+        this.peerInfo = peerInfo;
+        try {
+            output = new ObjectOutputStream(this.socket.getOutputStream());
+            input = new ObjectInputStream(this.socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void run() {
+        //handshake
+        output.writeObject(new Handshake);
+        try {
+            Handshake incomingHandshake = (Handshake)input.readObject();
+            remoteId = incomingHandshake.getIdField();
+            if(peerInfo.validPeerIds.contains(remoteId)){
+                receivedHandshake = true;
+            }
+            //TODO: close connection and log rejection if the handshake fails.
+            if(receivedHandshake){
+                if(peerInfo.hasFile) {
+                    //decide where we're storing the bitfields of the local peer and remote peers
+                    //output.writeObject(new Bitfield());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
         //while true
             //listen for message
     }
--- src/PeerInfo.java
+++ src/PeerInfo.java
@@ -5,7 +5,7 @@ public class PeerInfo {
     public final String hostname;
     public final int peerId;
     public final Set<Integer> validPeerIds;
-    private boolean hasFile;
+    public boolean hasFile;
 
     public PeerInfo(int port, String hostname, int peerId, Set<Integer> validPeerIds) {
         this.port = port;
