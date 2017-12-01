cb7d8b702f510024ce5abe2480ec67ef30f85f29
Brenton Lin <notnerb.94@ufl.edu>
2017-12-01 16:28:13 -0500

    refactored Unchoker

--- src/ChokerUnchoker.java
+++ src/ChokerUnchoker.java
@@ -1,58 +1,34 @@
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.rmi.Remote;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class ChokerUnchoker{
-    private static PeerInfo peerInfo;
-  public ChokerUnchoker(PeerInfo _peerInfo) {
-     peerInfo = _peerInfo;
-   }
-
-    public static void main(String[] args)
-    {
-
-            Timer timer = new Timer();
-            TimerTask task = new TaskHelper(peerInfo);
-            timer.schedule(task, 2000, 5000);
 
 
-    }
-    //keeping it just in case
-    public void registerRemotePeer(Integer pID, ObjectOutputStream output, ObjectInputStream input){
-
-    }
-}
-
-class TaskHelper extends TimerTask {
+public class ChokerUnchoker extends TimerTask {
     private PeerInfo localPeerInfo;
-    private HashMap localRemotePeers;
-    private RemotePeerInfo[] bytesRecieved;
-  public TaskHelper (PeerInfo peerInfo) {
-        super();
+    private ArrayList<RemotePeerInfo> remotePeers;
+  public ChokerUnchoker (PeerInfo peerInfo) {
+      super();
       localPeerInfo = peerInfo;
-      localRemotePeers  = localPeerInfo.remotePeers;
+      remotePeers = new ArrayList<RemotePeerInfo>();
       //initiliaze bytes recieved array;
-      bytesRecieved = new RemotePeerInfo[localRemotePeers.size()];
-      Integer i = 0;
       //iterate over the localremotePeers hashmap and create and entry in bytesrecieved for each;
-      for(Object pID : localRemotePeers.keySet()){
-          bytesRecieved[i] = (RemotePeerInfo)localRemotePeers.get(pID);
-          i++;
-      }
-      //
+
   }
   //function for sorting the bytes recieved array every execute cycle.
-  public void sortBytesRecieved(RemotePeerInfo[] _bytesRecieved){
-      int maxBytes = 0;
-
-  }
 
     public static int i = 0;
     public void run() {
         System.out.println("Timer ran " + ++i);
+        remotePeers.clear();
+        for(int pID : localPeerInfo.remotePeers.keySet()){
+            remotePeers.add(localPeerInfo.remotePeers.get(pID));
+        }
+        //
     }
 }
 
